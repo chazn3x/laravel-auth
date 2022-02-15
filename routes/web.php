8b945d10bc20c31c13d748cb48c7eb2ed599b4ac
Giovanni <gio.a.98@gmail.com>
2022-02-15 15:14:40 +0100

    added admin routes

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
+    
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostsController');
+
+});
+
